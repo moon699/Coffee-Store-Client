6f4b2068d6eb3d22dd7f7a9958a015c831a658ea
Pritam Chowdhury <moon699>
2025-03-17 14:06:29 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
-
-import { useLoaderData } from 'react-router-dom'
-import './App.css'
-import CoffeeCard from './Component/CoffeeCard'
+import { useLoaderData } from "react-router-dom";
+import "./App.css";
+import CoffeeCard from "./Component/CoffeeCard";
+import { useState } from "react";
 
 function App() {
-
-
-      const coffees =useLoaderData()
-
+  const loaderCoffees = useLoaderData();
+  const [coffees, setCoffees] = useState(loaderCoffees);
 
   return (
-
     <>
-      
-      <div className='m-20'>
-        <h1 className='text-6xl text-center my-20 text-purple-600'>Hot cold coffees:{coffees.length}</h1>
-      <div className='grid md:grid-cols-2 gap-4'>
-        {
-        coffees.map(coffee => <CoffeeCard
-          key={coffee._id}
-          coffee={coffee}
-        ></CoffeeCard>)
-      }
+      <div className="m-20">
+        <h1 className="text-6xl text-center my-20 text-purple-600">
+          Hot cold coffees:{coffees.length}
+        </h1>
+        <div className="grid md:grid-cols-2 gap-4">
+          {coffees.map((coffee) => (
+            <CoffeeCard
+              key={coffee._id}
+              coffee={coffee}
+              coffees={coffees}
+              setCoffees={setCoffees}
+            ></CoffeeCard>
+          ))}
+        </div>
       </div>
-      </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Component/CoffeeCard.jsx
+++ src/Component/CoffeeCard.jsx
@@ -1,67 +1,70 @@
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 
-const CoffeeCard = ({coffee}) => {
+const CoffeeCard = ({ coffee, coffees, setCoffees }) => {
+  const { _id, name, quantity, supplier, taste, category, details, photo } =
+    coffee;
 
-  const { _id, name, quantity, supplier, taste, category, details, photo } = coffee;
-
-  const handleDelete = _id =>{
+  const handleDelete = (_id) => {
     console.log(_id);
     Swal.fire({
-  title: "Are you sure?",
-  text: "You won't be able to revert this!",
-  icon: "warning",
-  showCancelButton: true,
-  confirmButtonColor: "#3085d6",
-  cancelButtonColor: "#d33",
-  confirmButtonText: "Yes, delete it!"
-}).then((result) => {
-  if (result.isConfirmed) {
-    
-
-    fetch(`http://localhost:5000/coffee/${_id}`,{
-      method:'DELETE'
-    })
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-        if (data?.deletedCount > 0) {
-          Swal.fire({
-          title: "Deleted!",
-          text: "Your Coffee has been deleted.",
-          icon: "success"
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data?.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your Coffee has been deleted.",
+                icon: "success",
+              });
+              const remaining = coffees.filter(coffee._id !== _id);
+              setCoffees(remaining);
+            }
           });
-        }
-    })
+      }
+    });
+  };
 
-  }
-});
-};
-
-    return (
-        <div className="card card-side bg-base-100 shadow-xl">
-        <figure>
-          <img src={photo} alt="Movie" className="w-32 h-32 object-cover" />
-        </figure>
-            <div className="flex justify-between w-full pr-5">
-          <div>
-            <h2 className="card-title">name:{name}</h2>
+  return (
+    <div className="card card-side bg-base-100 shadow-xl">
+      <figure>
+        <img src={photo} alt="Movie" className="w-32 h-32 object-cover" />
+      </figure>
+      <div className="flex justify-between w-full pr-5">
+        <div>
+          <h2 className="card-title">name:{name}</h2>
           <p>quantity:{quantity}</p>
           <p>supplier:{supplier}</p>
           <p>taste:{taste}</p>
-          </div>
-          <div className="card-actions justify-end">
-            <div className="flex flex-col space-y-4">
-    <button className="btn btn-primary">VIEW</button>
-              <Link to={`updateCoffee/${_id}`}>
-                    <button className="btn btn-secondary">EDIT</button>
-              </Link>
-              <button onClick={() => handleDelete(_id)} className="btn btn-accent">X</button>
-  </div>
-</div>
-            </div>
         </div>
-    );
-
-  }
+        <div className="card-actions justify-end">
+          <div className="flex flex-col space-y-4">
+            <button className="btn btn-primary">VIEW</button>
+            <Link to={`updateCoffee/${_id}`}>
+              <button className="btn btn-secondary">EDIT</button>
+            </Link>
+            <button
+              onClick={() => handleDelete(_id)}
+              className="btn btn-accent"
+            >
+              X
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default CoffeeCard;
